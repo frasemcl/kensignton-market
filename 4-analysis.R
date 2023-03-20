651de9bb7f83aa38c16c56da620723ee653c4acc
Fraser McLaughlin <fraser.mclaughlin@outlook.com>
2023-03-20 17:24:03 -0400

    making progress on first plotly graphs

--- 4-analysis.R
+++ 4-analysis.R
@@ -10,33 +10,30 @@ library(leaflet)
 
 # This is a nice feature, TODO get the VAR details
 # Use this to look at what may be comparable in prev years
-list_census_vectors('CA21')
+census_vectors_21 <- list_census_vectors('CA21')
+
+# From the previous scripts:
+tor_census_df
+
+# Shorten colnames (will build a lookup table with better info on each code)
+colnames(tor_census_df) <- c(str_split(colnames(tor_census_df), ":") %>% map_chr(`[`, 1))
 
 
-# fig <- plot_ly(ggplot2::diamonds, x = ~cut, y = ~price, color = ~clarity, type = "box")
-# fig <- fig %>% layout(boxmode = "group")
-# fig
-
-
-test <- tor_census_df %>% 
-  select(c(1:3,17:26))
-
-# TODO How to fill NAs? 
-test <- drop_na(test)
-
-# This if reasonable to add zeros
-# temp2[is.na(temp2)] <- 0
-
+# TODO How to fill NAs more appropirately? For now using -999
+# tor_census_df <- drop_na(tor_census_df)
+tor_census_df[is.na(tor_census_df)] <- -999
 # Subsetting while testing
-# test <- test[1:10,]
+# tor_census_df <- tor_census_df[1:10,]
+
+
+
 
 # How to summarize?
-
-test2 <- summary(test)
-
+summary_tor_census_df <- summary(tor_census_df)
 # A quick sumtable() is a nice look:
 # https://cran.r-project.org/web/packages/vtable/vignettes/sumtable.html
-st(test)
+# Hmm this worked the first time....
+st(tor_census_df)
 
 
 ##### Leaflet choropleth
@@ -57,49 +54,63 @@ st(test)
 
 ########## Plotly histogram for all variables 
 # Select the variables of interest
-var_names <- colnames(test)
-# Edit this when I start working with the true DF
-var_names <- tail(var_names, -3)
+var_names <- colnames(tor_census_df)
+
+
+# The first 16 variables don't contain census data
+# TODO make this selection less manual
+var_names <- tail(var_names, -16)
 var_names
 # Check, to delete later:
 var_names[1]
 var_names[length(var_names)]
 
 # This function creates the dropdown options
+# Logic to change title: https://community.plotly.com/t/changing-the-xy-axis-titles-with-dropdown-menu/5421/2
+# And: https://plotly.com/r/dropdowns/
+# Still not working, put on pause
+# This should get me there! :
+# https://plotly.com/r/custom-buttons/?_gl=1*1ovco31*_ga*NDc0MDczMDYuMTY3NjA0MDU4MQ..*_ga_6G7EE0JNSC*MTY3OTM0MzUzNy4yMC4xLjE2NzkzNDM3MDIuMC4wLjA.
 create_dropdown <- function(df, vars) {
   lapply(
     vars,
     FUN = function(var_name, df) {
       button <- list(
         method = 'restyle',
-        args = list('x', list(df[, var_name])),
+        args = list('x', list( df[, var_name])),
+        # args = list(
+        #     list(list(x =list(df[, var_name])), list(xaxis = list(title = "test")))
+        #     ),
         label = var_name
       )
     },
     df
   )
-  
 }
+
+
+# TODO shorten dropdown labels, update x axis title 
 
 # Subset based on variable column name (testing123)....delete later
 # test[, var_names[1]]
 
 # making the histogram(s)
-fig <- plot_ly(x = test[, var_names[1]], type = "histogram", 
+fig <- plot_ly(x = tor_census_df[, var_names[1]], type = "histogram", 
                showlegend=FALSE,
                name='Histogram',
                marker = list(line = list(color = 'rgb(8,48,107)',
-                                         width = 1.5))) %>% 
-  layout(title='test', xaxis = list(title = var_names[1]),
+                                         width = 1.5))) %>%   
+  layout(xaxis = list(title = 'census var'),
          yaxis = list(title = 'n census tracts'),
          updatemenus = list(
            list(
-             y = 0.7,
-             buttons = create_dropdown(test, var_names)
+             # y = 0.7,
+             buttons = create_dropdown(tor_census_df, var_names)
            )
           )
   )
 fig
+
 
 
 
@@ -136,30 +147,8 @@ fig2
 
 
 ############# Archive 
-#  buttons were made manually before using a function:
-fig <- plot_ly(x = test[, var_names[1]], type = "histogram", 
-               showlegend=FALSE,
-               name='Histogram',
-               marker = list(line = list(color = 'rgb(8,48,107)',
-                                         width = 1.5))) %>% 
-  layout(title='test', xaxis = list(title = var_names[1]),
-         yaxis = list(title = 'n census tracts'),
-         updatemenus = list(
-           list(
-             y = 0.7,
-             buttons = list(
-               list(method = "restyle",
-                    args = list("x", list(test[, var_names[1]]),
-                                list(yaxis = list(title = var_names[1])),
-                    ),
-                    label = var_names[1]),
-               list(method = "restyle",
-                    args = list("x", list(test[, var_names[2]]),
-                                list(yaxis = list(title = var_names[2]))),
-                    label = var_names[2])
-             )))
-  )
-fig
+
+
 
 # Helpful but I'm not quite there yet (combining the two histograms...)
 # https://stackoverflow.com/questions/40024029/plotly-updating-data-with-dropdown-selection
